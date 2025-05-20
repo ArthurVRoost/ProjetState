f377156b6753089d333934038ac90b78f62524e8
Arthur Van Roost <arthur.vanroost@icloud.com>
2025-05-20 10:25:03 +0200

    form avancement + merge appjsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 
 import './App.css'
 import Card from './Components/card/Card'
+import Form from './Components/form/Form'
 
 function App() {
   
 
   return (
     <>
-      <div>
-        <Card/>
+      <div className='divApp'>
+        <div className='appCard'>
+          <Card/>
+        </div>
+        <div className='appReste'>
+          <Form/>
+        </div>
+
       </div>
       
     </>
--- src/Components/form/Form.jsx
+++ src/Components/form/Form.jsx
@@ -6,21 +6,19 @@ export default function Form() {
     <>
       <div className='divForm'>
             <div className='formDiv1'>
-                <h1>Personal info</h1>
-                <p>Please provide your name, your email address, and phone number</p>
+                <h1 className='formH1'>Personal info</h1>
+                <p className='formP'>Please provide your name, your email address, and phone number</p>
             </div>
             <div className='formDiv2'>
                 <form>
-                    <label htmlFor="name">Your name is :</label>
-                    <input type="text" id="name" name="name" placeholder="Vingt-Six" required />
+                    <label className='labelName' htmlFor="name">Your name is :</label>
+                    <input className='inputName' type="text" id="name" name="name" placeholder="Vingt-Six" required />
 
-                    <label htmlFor="email">Your email is :</label>
-                    <input type="email" id="email" name="email" placeholder="vingt_six@email.com" required />
+                    <label className='labelEmail' htmlFor="email">Your email is :</label>
+                    <input className='inputEmail' type="email" id="email" name="email" placeholder="vingt_six@email.com" required />
 
-                    <label htmlFor="phone">Your phone number is :</label>
-                    <input type="tel" id="phone" name="phone" placeholder="e.g.+1 234 567 890" required />
-
-                    <button type="submit">Submit</button>
+                    <label className='labelPhone' htmlFor="phone">Your phone number is :</label>
+                    <input className='inputPhone' type="tel" id="phone" name="phone" placeholder="e.g.+1 234 567 890" required />
                 </form>
             </div>
             <div className='formDiv3'>
