81086cae873011c8e10ca33a68bc4fbe751f269e
Arthur Van Roost <arthur.vanroost@icloud.com>
2025-05-20 14:19:46 +0200

    div thanks html

---
+++ src/Components/thanks/Thanks.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './thanks.css'
+import PhotoThanks from '/src/assets/img/thankyou.svg'
+
+export default function Thanks() {
+    return(
+        <>
+        <div className='divThanks'>
+            <img className='thanksImg' src={PhotoThanks} alt="" />
+            <h1 className='thanksH1'>Thank You!</h1>
+            <p className='thanksP'>Thanks for confirming your subscription! We hope you have fun using our platform. If you ever need support, please feel free to email us at support@loremgaming.com </p>
+        </div>
+        </>
+    )
+}
