d760c6b8440f4131837bde963e2abb911572b4fb
Arthur Van Roost <arthur.vanroost@icloud.com>
2025-05-20 12:30:06 +0200

    avancement addon

--- src/Components/add/Add.jsx
+++ src/Components/add/Add.jsx
@@ -13,31 +13,31 @@ export default function Add() {
           <p className="formP">Add-ons help enhance your gaming experience</p>
         </div>
         <div className="addons-container">
-          <div className="addon-card"style={{backgroundColor: online ? "hsl(229, 24%, 87%)" : "#ffffff",}}onClick={() => setOnline(!online)}>
+          <div className="addon-card"style={{backgroundColor: online ? "hsla(230, 13.00%, 91.00%, 0.65)" : "#ffffff",}}onClick={() => setOnline(!online)}>
                 <input type="checkbox" checked={online} onChange={() => setOnline(!online)} onClick={(e) => e.stopPropagation()}/>
                 <div className="addon-text">
                 <h3>Online service</h3>
                 <p>Access to multiplayer games</p>
                 </div>
-                <span className="price">+$10/yr</span>
+                <span className="price1">+$10/yr</span>
           </div>
 
-          <div className="addon-card" style={{backgroundColor: storage ? "hsl(229, 24%, 87%)" : "#ffffff",}} onClick={() => setStorage(!storage)}>
+          <div className="addon-card" style={{backgroundColor: storage ? "hsla(230, 13.00%, 91.00%, 0.65)" : "#ffffff",}} onClick={() => setStorage(!storage)}>
                 <input type="checkbox" checked={storage} onChange={() => setStorage(!storage)} onClick={(e) => e.stopPropagation()}/>
                 <div className="addon-text">
                 <h3>Larger storage</h3>
                 <p>Extra 1TB of cloud save</p>
                 </div>
-                <span className="price">+$20/yr</span>
+                <span className="price2">+$20/yr</span>
           </div>
 
-          <div className="addon-card" style={{backgroundColor: profile ? "hsl(229, 24%, 87%)" : "#ffffff",}}onClick={() => setProfile(!profile)}>
+          <div className="addon-card" style={{backgroundColor: profile ? "hsla(230, 13.00%, 91.00%, 0.65)" : "#ffffff",}}onClick={() => setProfile(!profile)}>
                 <input type="checkbox" checked={profile} onChange={() => setProfile(!profile)} onClick={(e) => e.stopPropagation()}/>
                 <div className="addon-text">
                 <h3>Customizable Profile</h3>
                 <p>Custom theme on your profile</p>
                 </div>
-                <span className="price">+$20/yr</span>
+                <span className="price3">+$20/yr</span>
           </div>
         </div>
       </div>
